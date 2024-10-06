cc19f6e530ce6e5f5ede2d10b44fd9e7d3f51334
cagongu <hophucthai9@gmail.com>
2024-10-06 09:14:46 +0700

    start creating project

---
+++ src/main/java/com/spring6framework/ChuTro/ChuTroApplication.java
@@ -0,0 +1,13 @@
+package com.spring6framework.ChuTro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuTroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuTroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring6framework/ChuTro/ChuTroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring6framework.ChuTro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuTroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
